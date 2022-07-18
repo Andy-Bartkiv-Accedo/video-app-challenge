6dac164bb9ab75548017082af914047e99eeb3d8
Andy Bartkiv <andy.bartkiv@accedo.tv>
2022-07-18 16:01:43 -0400

    extract Dots component, implement useMemo and useCallback for calculations

--- src/components/UI/carousel/Carousel.tsx
+++ src/components/UI/carousel/Carousel.tsx
@@ -1,43 +1,59 @@
-import { useState } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import cls from './Carousel.module.css';
 import { useWindowDimensions } from '../../../hooks/useWindowDimensions';
+import cls from './Carousel.module.css';
 import Item from './Item';
 import Button from './Button';
+import Dots from './Dots';
 
 interface Props {
-    items: any[]
+    items: MediaItem[]
 };
+const rotateFn: string = 'rotateY'; // rotateX for different rotation axis
 
 const Carousel: React.FC<Props> = ({ items }) => {
 
     const navigate = useNavigate();
     
-    const dig: number = Math.floor(Math.random() * items.length) // init starting index for carousel
-    const [val, setVal] = useState<number>(dig); // position of carousel HEAD element
-    const rotateFn: string = 'rotateY'; // rotateX for different rotation axis
-    const cellCount: number = items.length; // number of cells in carousel
-    const { height, width } = useWindowDimensions();
-    const cellSize: number = width * 0.8;
-    const radius: number = Math.round(( cellSize / 2) / Math.tan( Math.PI / cellCount ));
+    const [val, setVal] = useState<number>(Math.floor(Math.random() * items.length)); // position of carousel HEAD element
 
+    // calculating Carousel Radius based on each Cell Size and Cell Q-ty = items.length
+    const cellSize: number = useWindowDimensions().width * 0.8;
+    const radius: number = useMemo(() => 
+        Math.round(( cellSize / 2) / Math.tan( Math.PI / items.length ))
+    , [cellSize, items.length]);
+    // perspective distance
+    const perspective: number = useMemo(() =>
+        2 * radius * Math.cos( Math.PI / items.length )
+    , [radius, items]);
+    console.log(perspective/radius)
     // 3D positioning each individual cell
-    const cellsStyle = items.map((item, index) => ({ 
-        transform: `${rotateFn}(${index*360/cellCount}deg) translateZ(${radius}px)`,
-    }));
+    const cellsStyle: string[] = useMemo(() =>
+        items.map((item, index) => 
+            `${rotateFn}(${index*360/items.length}deg) translateZ(${radius}px)`)
+    , [radius, items]);
 
     // carousel rotation animation
-    const rotateCar = (val: number): string => 
-        `translateZ(${-radius}px) ${rotateFn}(${(val/cellCount*-360)}deg)`;
+    const rotateCar = useCallback((val: number): string => 
+        `translateZ(${-radius}px) ${rotateFn}(${(val/items.length*-360)}deg)`
+    , [radius, items.length]);
 
-    const normalizeHead = (val: number): number =>
+    // normalize Head Position (to be in range: from 0 to item.length-1) 
+    const normHeadPosition = useCallback((val: number): number =>
         (val >= 0)
-            ? val % cellCount
-            : (cellCount + val % cellCount ) % cellCount;
+            ? val % items.length
+            : (items.length + val % items.length ) % items.length
+    , [items.length]);
 
-    // Carousel rotation click handle
-    function handleRotation(fwd: boolean = true): void {
+    // Arrows click Carousel rotation click handle
+    function handleArrowClick(fwd: boolean = true): void {
         const newVal = (fwd) ? val+1 : val-1;
+        setVal(newVal);
+    }
+
+    // Dot click Carousel rotation click handle
+    function handleDotClick(index: number): void {
+        const newVal = index + items.length * Math.floor(val/items.length)
         setVal(newVal);
     }
 
@@ -45,18 +61,18 @@ const Carousel: React.FC<Props> = ({ items }) => {
         <div className={ cls.container }>
             
             {/* Rotate left button */}
-            <Button direction='left' onClick={ () => handleRotation(false) }/>
+            <Button direction='left' onClick={ () => handleArrowClick(false) }/>
 
             {/* CAROUSEL */}
-            <div className={ cls.scene } style={{ perspective: `${radius*1.732}px` }}>
+            <div className={ cls.scene } style={{ perspective: `${perspective}px` }}>
                 <div className={ cls.carousel } style={{ transform: rotateCar(val) }}>
                     { items.map((item, index) =>
                         <Item key={ item.id }
                             title={ item.title }
-                            transform={ cellsStyle[index].transform }
-                            opacity={ (index === normalizeHead(val)) ? '1' : '.25' }
+                            transform={ cellsStyle[index] }
+                            opacity={ (index === normHeadPosition(val)) ? '1' : '.25' }
                             imgUrl={ `https://image.tmdb.org/t/p/original${item.background}` }
-                            onClick = {() => { navigate(`/details/${item.id}`)} }
+                            onClick ={ () => { navigate(`/details/${item.id}`)} }
                         />
                     )}
                 </div>
@@ -64,21 +80,15 @@ const Carousel: React.FC<Props> = ({ items }) => {
             </div>
 
             {/* Rotate right button */}
-            <Button direction='right' onClick={ () => handleRotation(true) }/>
+            <Button direction='right' onClick={ () => handleArrowClick(true) }/>
 
 
             {/* Navigation Dots at the bottom */}
-            <div className={ cls.dot_bar }>
-                { items.map((item, index) =>
-                    <span key={index}
-                        onClick = { () => 
-                            setVal(index + cellCount * Math.floor(val/cellCount)) } 
-                        className={ (index === normalizeHead(val)) 
-                            ? `${cls.dot} ${cls.current_dot}`
-                            : `${cls.dot}` }>
-                    </span>
-                )}
-            </div>
+            <Dots 
+                items= { items }
+                headPosition={ normHeadPosition(val) }
+                onClick={ handleDotClick }
+            />
 
         </div>
     )
